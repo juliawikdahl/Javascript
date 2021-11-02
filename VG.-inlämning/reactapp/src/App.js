2fb30f4aeeb2df5317d0e862f66b6edf3d8b837e
Julia Wikdahl <julia.wikdahl@utb.ecutbildning.se>
2021-11-02 13:28:34 +0100

    fel

--- VG.-inlämning/reactapp/src/App.js
+++ VG.-inlämning/reactapp/src/App.js
@@ -3,9 +3,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Customer from './views/Customer';
 import Form from './views/Form'; 
 import Navbar from './navbar/Navbar'; 
-import Card from './components/Card';
+
+import Card from '.components/Card'
 import {useState, useEffect} from 'react'
 
+function App() {
+
+  const [customers, setCostumers] = useState ([])
+  useEffect(() => {
+
+    async function fetchData () {
+    const res = await fetch ("https://ecexam-webapi.azurewebsites.net/api/Customers")
+    setCostumers(await res.json())
+    }
+    fetchData()
+  }, []) 
 const App = () => {
   return (
     <>
@@ -13,6 +25,17 @@ const App = () => {
     <Navbar />
       <Switch>
         <Route path="/customer" exact component={Customer}/>
+        <div className="container mt-5">
+           <div className ="row row-cols-1 row-cols-md-2 g-4">
+             {
+               customers.map(customer => (
+                 <div key={customer.id}  className="col">
+                    <Customer item={customer} />
+                 </div>
+               ))
+             }
+          </div> 
+         </div>
         <Route path="/" exact component={Form}/>
       </Switch>
     </BrowserRouter>
@@ -20,5 +43,5 @@ const App = () => {
   )
 }
 
-export default App
 
+export default App
--- VG.-inlämning/reactapp/src/components/CardGrid.js
+++ VG.-inlämning/reactapp/src/components/CardGrid.js
@@ -1,37 +1,27 @@
-import React from 'react'
-import Card from '.components/Card'
+// import React from 'react'
+// import Card from '.components/Card'
 
-const CardGrid = () => {
+// const CardGrid = () => {
       
-function App() {
-
-    const [customers, setCostumers] = useState ([])
-    useEffect(() => {
-  
-      async function fetchData () {
-      const res = await fetch ("https://ecexam-webapi.azurewebsites.net/api/Customers")
-      setCostumers(await res.json())
-      }
-      fetchData()
-    }, [])  
+ 
     
-    return (
-        <div>
-            return (
-                <Card />
-           <div className="container mt-5">
-           <div className ="row row-cols-1 row-cols-md-2 g-4">
-             {
-               customers.map(customer => (
-                 <div key={customer.id}  className="col">
-                    <Customer item={customer} />
-                 </div>
-               ))
-             }
-          </div> 
-         </div>
-        </div> 
-    )
-}
+//     return (
+//         <div>
+//             return (
+//                 <Card />
+//            <div className="container mt-5">
+//            <div className ="row row-cols-1 row-cols-md-2 g-4">
+//              {
+//                customers.map(customer => (
+//                  <div key={customer.id}  className="col">
+//                     <Customer item={customer} />
+//                  </div>
+//                ))
+//              }
+//           </div> 
+//          </div>
+//         </div> 
+//     )
+// }
 
-export default CardGrid
+// export default CardGrid
